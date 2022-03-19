c02641cf131d05369165d7ac57a0b67116fbf9a3
yjclarelee <yjclarelee@postech.ac.kr>
2022-03-20 05:05:03 +0900

    Implementation of selection sort

---
+++ selection_sort.py
@@ -0,0 +1,11 @@
+def selection_sort(arr):
+  for i in range(len(arr)):
+    min_idx = i
+    for j in range(i+1, len(arr)):
+      if arr[min_idx] > arr[j]:
+        min_idx = j
+    arr[i], arr[min_idx] = arr[min_idx], arr[i]
+  return arr
+
+print(selection_sort([9, 2, 4, 1]))
+    
